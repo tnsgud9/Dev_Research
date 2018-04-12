ef999af0deed3a608ba292d7342588fb46cd4ea7
tnsgud9 <tnsgud9@naver.com>
2018-04-12 21:49:42 +0900

    아두이노 카테고리 추가
    + LED ON  OFF 소스 추가

---
+++ Arduino/LED_On_Off.ino
@@ -0,0 +1,36 @@
+//아두이노 LED ON OFF
+//빨노초 신호등 신호처리 코드
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
+
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13,HIGH);
+  digitalWrite(9,HIGH);
+  delay(2000);
+  digitalWrite(13,LOW);
+  digitalWrite(9,LOW);
+
+  
+  digitalWrite(12,HIGH);
+  digitalWrite(8,HIGH);
+  delay(1000);
+  digitalWrite(12,LOW);
+
+  
+  digitalWrite(11,HIGH);
+  delay(2000);
+  digitalWrite(11,LOW);
+  digitalWrite(8,LOW);
+
+}
+
+
