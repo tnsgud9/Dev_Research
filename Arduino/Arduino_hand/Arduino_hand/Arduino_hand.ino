4429d729c4993f37bf680cac64d11b455e509204
tnsgud9 <tnsgud9@naver.com>
2018-04-20 22:30:52 +0900

    아두이노 의수 프로젝트 시작

---
+++ Arduino/Arduino_hand/Arduino_hand/Arduino_hand.ino
@@ -0,0 +1,15 @@
+﻿/*
+ Name:		Arduino_hand.ino
+ Created:	2018-04-20 오후 10:30:14
+ Author:	user
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
