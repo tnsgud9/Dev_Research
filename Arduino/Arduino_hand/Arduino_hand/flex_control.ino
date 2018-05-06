b16fdad8c7d01db5eb4cfee2957c92888af6e5b1
tnsgud9 <tnsgud9@naver.com>
2018-05-06 22:29:16 +0900

    update flex_control

--- Arduino/Arduino_hand/Arduino_hand/flex_control.ino
+++ Arduino/Arduino_hand/Arduino_hand/flex_control.ino
@@ -13,11 +13,11 @@ Author:	user
 SoftwareSerial mySerial(1, 0);
 
 //pinMode set
-int f_sensor_0=0;
-int f_sensor_1=1;
-int f_sensor_2=2;
-int f_sensor_3=3;
-int f_sensor_4=4;
+int f_sensor0=0;
+int f_sensor1=1;
+int f_sensor2=2;
+int f_sensor3=3;
+int f_sensor4=4;
 
 
 // the setup function runs once when you press reset or power the board
@@ -28,11 +28,11 @@ void setup() {
 	Serial.begin(9600);
 
 	//flex sensor line
-	pinMode(6, INPUT);
-	pinMode(5, INPUT);
-	pinMode(4, INPUT);
-	pinMode(3, INPUT);
-	pinMode(2, INPUT);
+	pinMode(6, OUTPUT);
+	pinMode(5, OUTPUT);
+	pinMode(4, OUTPUT);
+	pinMode(3, OUTPUT);
+	pinMode(2, OUTPUT);
 
 
 	while (1) //시리얼 통신 연결
@@ -54,8 +54,24 @@ void loop() {
 
 	if (mySerial.available())
 	{
+		//Flex Sensor input
+		f_sensor0 = analogRead(A0);
+		f_sensor1 = analogRead(A1);
+		f_sensor2 = analogRead(A2);
+		f_sensor3 = analogRead(A3);
+		f_sensor4 = analogRead(A4);
 
+
+		Serial.print(F_sensor_0);
+		Serial.print(F_sensor_1);
+		Serial.print(F_sensor_2);
+		Serial.print(F_sensor_3);
+		Serial.print(F_sensor_4);
 	}
+
+
+
+
 
 
 }
