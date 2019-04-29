1e0d1dece2c8b43398b42cc7b0ba12b35f8014de
SoonHyung_Lee <tnsgud9@naver.com>
2019-04-30 00:55:25 +0900

    add

---
+++ C,C++ Algorithm/C,C++ Algorithm/2579.cpp
@@ -0,0 +1,21 @@
+//http://boj.kr/2579
+//계단 오르기
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int main()
+{
+	int n; //몇번 입력 받을지
+	cin >> n;
+	vector<int> stair(n + 4); // vector로 동적 배열 생성
+	vector<int> dp(n + 4);
+	for (int i = 1+3; i <= n+3; i++) cin >> stair[i];
+
+	for (int i = 1+3; i <= n+3; i++)
+	{
+		dp[i] = max(stair[i] + stair[i - 1] + dp[n - 3], stair[i] + dp[n - 2]);
+	}
+	cout << dp[n+3];
+}
